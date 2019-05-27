cfb4f64327bd979d291dce7a21254fa74037496e
Bl4X <Niclas199@gmx.de>
2019-05-27 15:27:45 +0200

    Rerestart
    .

---
+++ NC_Wasteland/Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        moveSpeed = 3;
+        jumpForce = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed, myRigidbody.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        }
+    }
+}
