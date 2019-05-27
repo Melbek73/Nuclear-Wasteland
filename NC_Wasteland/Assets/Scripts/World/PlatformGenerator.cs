b20812aced3d929c0f9009cbde8345a89f8ddce9
Bl4X <Niclas199@gmx.de>
2019-05-27 19:39:04 +0200

    add resources folder
    .

--- NC_Wasteland/Assets/Scripts/World/PlatformGenerator.cs
+++ NC_Wasteland/Assets/Scripts/World/PlatformGenerator.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    public GameObject thePlatform;
+    public GameObject lastPlatform;
     public Transform generationPoint;
     public float distanceBetween;
 
     private float platformWidth;
+    private GameObject prefabPlatform;
 
     // Start is called before the first frame update
     void Start()
     {
-        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+        prefabPlatform = (GameObject)Resources.Load("Prefabs/Worldgeneration/Level1/Level1_0", typeof(GameObject));
+        platformWidth = lastPlatform.GetComponent<BoxCollider2D>().size.x;
+        //Debug.Log("platform width:" + platformWidth);
+
+        transform.position = new Vector2(transform.position.x + platformWidth + distanceBetween, lastPlatform.transform.position.y);
+        Instantiate(lastPlatform, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
@@ -21,9 +27,9 @@ public class PlatformGenerator : MonoBehaviour
     {
         if (transform.position.x < generationPoint.position.x)
         {
-            transform.position = new Vector2(transform.position.x + platformWidth + distanceBetween, thePlatform.transform.position.y);
-
-            Instantiate(thePlatform, transform.position, transform.rotation);
+            transform.position = new Vector2(transform.position.x + platformWidth + distanceBetween, lastPlatform.transform.position.y);
+            lastPlatform.transform.position = transform.position;
+            Instantiate(prefabPlatform, transform.position, transform.rotation);
         }
     }
 }
