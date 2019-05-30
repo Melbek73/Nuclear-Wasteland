37fc27cfbbcd65dc992838792778620b435ab4b8
Melbek73 <49585657+Melbek73@users.noreply.github.com>
2019-05-30 17:14:03 +0200

    RPG kann SCHIEßEN

--- NC_Wasteland/Assets/Scripts/Player/GunRotation.cs
+++ NC_Wasteland/Assets/Scripts/Player/GunRotation.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class GunRotation : MonoBehaviour
 {
+    public Rigidbody2D rocket;              // Prefab of the rocket.
+    public float speed = 20f;				// The speed the rocket will fire at.
+    public static float bulletAngle = 0.0f;
+
+    private PlayerControl playerCtrl;       // Reference to the PlayerControl script.
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = transform.root.gameObject.GetComponent<Animator>();
+        playerCtrl = transform.root.GetComponent<PlayerControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 direction = myPos - target;
+        direction.Normalize();
+        float bulletAngle;
+
+        if (PlayerControl.facingRight == true)
+        {
+            bulletAngle = -Vector2.SignedAngle(direction, Vector2.right);
+        }
+        else
+        {
+            bulletAngle = -Vector2.SignedAngle(direction, Vector2.left);
+        }
+
+        transform.eulerAngles = new Vector3(0, 0, bulletAngle);
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            bulletAngle = -Vector2.SignedAngle(direction, Vector2.left);
+            Rigidbody2D bulletInstance = Instantiate(rocket, myPos, Quaternion.Euler(new Vector3(0, 0, bulletAngle))) as Rigidbody2D;
+            bulletInstance.velocity = -direction * speed;
+        }
     }
 }
